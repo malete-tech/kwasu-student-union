466d11b47a6dad7e4f4d666b33ba95f5e1644e83
Moshood Abdulhaqq Omotayo <moshoodoabdulhaqq@gmail.com>
2025-10-21 08:17:08 +0100

    [dyad] Aligned event cards on the public events page by making them fill the full height of their grid cells. - wrote 1 file(s)

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -109,9 +109,9 @@ const EventsPage: React.FC = () => {
             </div>
 
             {loading ? (
-              <div className="space-y-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {[...Array(4)].map((_, i) => (
-                  <div key={i} className="flex flex-col overflow-hidden shadow-lg rounded-xl">
+                  <div key={i} className="flex flex-col overflow-hidden shadow-lg rounded-xl h-full">
                     <div className="p-4 space-y-2">
                       <Skeleton className="h-6 w-3/4" />
                       <Skeleton className="h-4 w-1/2" />
@@ -130,7 +130,7 @@ const EventsPage: React.FC = () => {
             ) : filteredEvents.length > 0 ? (
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {filteredEvents.map((eventItem) => (
-                  <EventCard key={eventItem.id} event={eventItem} />
+                  <EventCard key={eventItem.id} event={eventItem} className="h-full" />
                 ))}
               </div>
             ) : (
